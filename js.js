f45eb2dcbff46a996a372766dfcd9204edcea4a3
Burdinova <iburdinova@yandex.ru>
2024-10-17 01:19:07 +0300

    2 section with slider

---
+++ js.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const banners = document.querySelectorAll(".banner__body");
+  const buttons = document.querySelectorAll(".banner__pagination-button");
+  const wrapper = document.querySelector(".banner__wrapper");
+  const newrapper = document.querySelector(".banner__test");
+  let currentIndex = 0;
+
+  function showBanner(index) {
+    banners.forEach((banner, i) => {
+      banner.classList.remove("active");
+      buttons[i].classList.remove("is-current");
+    });
+
+    banners[index].classList.add("active");
+    buttons[index].classList.add("is-current");
+  }
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      currentIndex = parseInt(e.target.getAttribute("data-index"));
+      console.log(currentIndex);
+      showSlide(currentIndex);
+      showBanner(currentIndex);
+      console.log(currentIndex);
+    });
+  });
+
+  function showSlide(index) {
+    wrapper.style.transform = `translateX(-${index *  newrapper.clientWidth}px)`;
+  }
+  showBanner(currentIndex);
+});
